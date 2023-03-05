567daad21c96e29de11bfb4ebf34d2091eecffaa
opuopu <43672649+opuopu@users.noreply.github.com>
2023-03-05 21:28:58 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-practice
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-erxtx4)
---
+++ index.js
@@ -0,0 +1,23 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+const arrays = [10, 20, 20, 30, 10, 20, 50, 40];
+
+function removeItem(array) {
+  const unique = [];
+  for (let i = 0; i <array.length; i++) {
+    if (unique.includes(array[i]) === false) {
+      unique.push(array[i]);
+      console.log(unique);
+    } else {
+    }
+  }
+  return unique;
+}
+
+removeItem(arrays);
+console.log(arrays.includes(10))
