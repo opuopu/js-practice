5d0c87ff3a255e525affde8fdb12ea898b8b01c0
opuopu <43672649+opuopu@users.noreply.github.com>
2023-03-05 21:35:01 +0600

    1 file modified

--- index.js
+++ index.js
@@ -6,24 +6,3 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 const arrays = [10, 20, 20, 30, 10, 20, 50, 40];
-
-function removeItem(array) {
-  const unique = [];
-  for (let i = 0; i < array.length; i++) {
-    if (unique.includes(array[i]) === false) {
-      unique.push(array[i]);
-      console.log(unique);
-    } else {
-    }
-  }
-  return unique;
-}
-
-removeItem(arrays);
-console.log(arrays.includes(10));
-console.log(arrays.includes(10));
-console.log(arrays.includes(10));
-console.log(arrays.includes(10));
-console.log(arrays.includes(10));
-console.log(arrays.includes(10));
-console.log(arrays.includes(10));
