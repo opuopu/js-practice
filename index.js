972db57fb0e86056203c55d4c5254bf14faaecfe
opuopu <43672649+opuopu@users.noreply.github.com>
2023-03-05 21:37:56 +0600

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Import stylesheets
-import './style.css';
+// import './style.css';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+// // Write Javascript code!
+// const appDiv = document.getElementById('app');
+// appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 const arrays = [10, 20, 20, 30, 10, 20, 50, 40];
+console.log('heloo');
